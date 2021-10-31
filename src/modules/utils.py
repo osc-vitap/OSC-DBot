1ccf0719e390b469ced9290157ecd07dabed9ebb
vijay <vijaybalajitheboss@gmail.com>
2021-11-01 01:20:02 +0530

    Setup a default message for unknown commands

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -3,8 +3,11 @@ import json
 def commands(message):
     with open ('data.json', 'r') as f:
         data = json.load(f)
+    response = "No command found. Use !help for more details"
     prefix = data['prefix']
+    
     for temp in data['commands'][0].keys():
         if message == prefix + temp:
             response = data["commands"][0][temp]
+            return response
     return response
