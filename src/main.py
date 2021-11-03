a05f13e3c09a00b15b5362f183c183748aa762f5
KodeLogger <nkrishnaraj0907@gmail.com>
2021-11-03 14:22:01 +0530

    fixed bot responding to user messages bug, refactored code

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ import discord
 import os
 
 if __name__ == "__main__":
-    load_dotenv()
-    TOKEN = os.getenv('DISCORD_TOKEN')
-    client = discord.Client()
+    load_dotenv()  # load .env file
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    client = discord.Client()  # init discord client
 
     @client.event
     async def on_ready():
-        print(f'{client.user} has connected to Discord!')
+        print(f"{client.user} has connected to Discord!")
         message_channel = client.get_channel(904455110212591676)
         oscEventNotif.start(message_channel)
 
@@ -23,4 +23,4 @@ if __name__ == "__main__":
         if response:
             await message.channel.send(response)
 
-    client.run(TOKEN)
+    client.run(TOKEN)  # run discord client
