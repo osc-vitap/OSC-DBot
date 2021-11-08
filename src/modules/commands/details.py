e959016e1e692c646c671ed884131ec2eef425b8
vijay <vijaybalajitheboss@gmail.com>
2021-11-09 01:36:00 +0530

    Setup contact embed

--- src/modules/commands/details.py
+++ src/modules/commands/details.py
@@ -1,6 +1,28 @@
+import discord
+
+
 class details:
     def contact():
-        pass
+        data = "Hey! OSC-DBot was made with **love** ❤️ by two developers from the **Open Source Community VIT-AP**"
+        embed = discord.Embed(
+            title="📧  Contact details",
+            description=data,
+            color=discord.Color.green(),
+        )
+        embed.add_field(
+            name="S Vijay Balaji",
+            value="vijaybalaji2477@gmail.com - <@410473185109344256>",
+            inline=False,
+        )
+        embed.add_field(
+            name="N Krishna Raj",
+            value="nkrishnaraj.developer@gmail.com - <@766903441816944690>",
+            inline=False,
+        )
+        embed.set_footer(
+            text="If developers are unavailable, you can drop a mail at osc@vitap.ac.in",
+        )
+        return embed
 
     def info():
         pass
