27141b609b40d102ccdbcaf8dc8ce8896f0c46a2
KodeLogger <nkrishnaraj0907@gmail.com>
2021-11-08 16:54:24 +0530

    fixed the bug, news data is synced to local json and sends latest news

--- src/modules/news_updates.py
+++ src/modules/news_updates.py
@@ -1,7 +1,7 @@
 from discord.ext import tasks, commands
 from urllib.request import urlopen
-from datetime import datetime
-import pyshorteners
+from datetime import date
+from pyshorteners import Shortener
 import discord
 import json
 
@@ -9,33 +9,101 @@ import json
 @tasks.loop(hours=12)
 async def news_updates(news_channel):
     urls = ["https://inshortsapi.vercel.app/news?category=technology"]
+
+    today = date.today().strftime("%d %b %Y")  # today date.
+    max_time = ""  # stores the latest news.
+
+    with open("data.json", "r") as f:
+        cache_mem = json.load(f)
+
+    # if last news was sent yesterday, then change data in the json file.
+    if today != cache_mem["newsTimestamp"].split("|")[0].strip():
+        cache_mem["newsTimestamp"] = today + " | " + "00:00 AM"
+        with open("data.json", "w") as f:
+            json.dump(cache_mem, f)
+
     for url in urls:
+        # get json data from the api
         response = urlopen(url)
         data = json.loads(response.read())["data"]
+        news_updates = dict()
+        news_count = 0
 
-        for i in range(2):
-            curr = data[i]
-            embed = discord.Embed(
-                title="📢  " + curr["title"],
-                url=curr["url"],
-                description=curr["content"],
-                color=discord.Color.blue(),
-            )
+        # get the news that were sent out today
+        for index in range(len(data)):
+            content = data[index]
+            if today == content["date"].split(",")[0]:
+                news_updates[news_count] = content
+                news_count += 1
 
-            embed.set_author(name="inshorts")
+        time_stamps = list()
 
-            shortener = pyshorteners.Shortener()
-            short_url = shortener.tinyurl.short(curr["readMoreUrl"])
+        # get the time of the news which were sent out today
+        for today_content in news_updates.values():
+            time_stamps.append(today_content["time"])
 
-            embed.add_field(
-                name="📰   Read more at ",
-                value=short_url,
-            )
+        # sort the time
+        time_stamps = sorted(time_stamps)
 
-            embed.set_image(url=curr["imageUrl"])
+        # check if the latest news is already sent out
+        for item in time_stamps.copy():
+            if item > cache_mem["newsTimestamp"].split(" | ")[1].strip():
+                break
+            time_stamps.remove(item)
 
-            date_and_time = "📰 " + curr["date"] + " " + curr["time"]
-            embed.set_footer(text=date_and_time)
-            response.close()
+        # if there is some update, then print and update the local json file.
+        if time_stamps != list():
+            for index in range(news_count):
+                for time_stamp in time_stamps:
+                    current_content = news_updates[index]
+                    if current_content["time"] == time_stamp:
+                        embed = discord.Embed(
+                            title="📢  " + current_content["title"],
+                            url=current_content["url"],
+                            description=current_content["content"],
+                            color=discord.Color.blue(),
+                        )
 
-            await news_channel.send(embed=embed)
+                        embed.set_author(name="inshorts")
+
+                        shortener = Shortener()
+                        short_url = shortener.tinyurl.short(
+                            current_content["readMoreUrl"]
+                        )
+
+                        embed.add_field(
+                            name="📰   Read more at ",
+                            value=short_url,
+                        )
+
+                        embed.set_image(url=current_content["imageUrl"])
+
+                        date_and_time = (
+                            "📰 "
+                            + current_content["date"]
+                            + " "
+                            + current_content["time"]
+                        )
+                        embed.set_footer(text=date_and_time)
+                        response.close()
+
+                        # prints outs log message
+                        print(
+                            "[!] Updating news feed: "
+                            + current_content["date"]
+                            + " | "
+                            + current_content["time"]
+                            + " -> "
+                            + current_content["title"]
+                        )
+
+                        # get the latest news time
+                        if max_time < current_content["time"]:
+                            max_time = current_content["time"]
+
+                        # update the latest time with the local json file
+                        cache_mem["newsTimestamp"] = today + " | " + max_time
+                        with open("data.json", "w") as f:
+                            json.dump(cache_mem, f, indent=4, separators=(",", ": "))
+
+                        await news_channel.send(embed=embed)
