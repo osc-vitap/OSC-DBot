8fb7b982159a1c72f1c478230ca4eabe41995eec
KodeLogger <nkrishnaraj0907@gmail.com>
2021-11-09 19:02:22 +0530

    added help command, needs footer icon fix though

--- src/modules/commands/details.py
+++ src/modules/commands/details.py
@@ -1,4 +1,5 @@
 import discord
+import json
 
 
 class details:
@@ -42,4 +43,76 @@ class details:
         return embed
 
     def help():
-        pass
+        with open("data.json", "r") as f:
+            data = json.load(f)
+        prefix = data["prefix"]
+
+        util_commands = f"""
+
+           > **{prefix}help**
+           > Lists all the **available commands**.
+           
+           > **{prefix}info**
+           > View **more information** on the bot. (Also feel free to contribute bruv)
+           
+           > **{prefix}contact**
+           > **Reveal the octocats** who made me.
+
+           > **{prefix}event**
+           > I hack OSC VIT-AP database and **fetch you the latest event**, please don't tell them I hacked their database.
+        
+        """
+
+        fun_commands = f"""
+
+           > **{prefix}joke**
+           > I tell a **Joke** and you must laugh.
+           
+           > **{prefix}meme**
+           > Get you **fresh memes** from the internet, Just memes.
+           
+           > **{prefix}quote**
+           > Fetch you a **random quote** from the internet.
+        
+        """
+
+        prefooter_content = f"""
+
+         For more help, cry. 
+         Just kidding, use **{prefix}info** to get access to more information on the bot.
+        
+         More commands coming soon.
+
+        """
+
+        data = """Are you lost little lamb? Fret not, following are the commands that you will need to memorize to use me."""
+        embed = discord.Embed(
+            title="📖  Help",
+            description=data,
+            color=discord.Color.blue(),
+        )
+        embed.add_field(
+            name="👉🏽 Utility Commands (For Noobs)",
+            value=util_commands,
+            inline=False,
+        )
+
+        embed.add_field(
+            name="👉🏽 Fun Commands (For Boomers)",
+            value=fun_commands,
+            inline=False,
+        )
+
+        embed.add_field(
+            name="👉🏽 More ...",
+            value=prefooter_content,
+            inline=False,
+        )
+
+        file = discord.File("assets/Icon.png", filename="logo.png")
+
+        embed.set_footer(
+            text=" The Open Source Community",
+            icon_url="attachment://logo.png",
+        )
+        return embed
